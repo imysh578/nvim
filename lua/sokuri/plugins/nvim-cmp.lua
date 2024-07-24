297ea20bef93f5db49afb28e3f91b2122bddcf8c
imysh578 <imysh578@gmail.com>
2024-07-24 19:44:19 +0900

    Feat: add tab for selection

--- lua/sokuri/plugins/nvim-cmp.lua
+++ lua/sokuri/plugins/nvim-cmp.lua
@@ -42,6 +42,24 @@ return {
         ["<C-i>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.exapnd_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
